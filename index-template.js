631a7ade7c934cdb614372a0cd21ac166a7165af
sukreshmanda <34400639+sukreshmanda@users.noreply.github.com>
2020-12-04 16:01:44 +0530

    Delete index-template.js

--- index-template.js
+++
@@ -1,7 +0,0 @@
-const core = require ('@actions/core')
-
-const name = core.getInput('who-to-wish',{default: 'Sukresh'})
-
-console.log("Hii Mr/Miss "+name)
-
-core.setOutput("statement","Hii Mr/Miss "+name)
